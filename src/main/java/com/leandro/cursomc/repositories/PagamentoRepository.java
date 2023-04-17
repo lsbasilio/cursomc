5c6cd3e1ec1df0618acd62dc90666fa9880053cf
lsbasilio <leandro_sanzi@yahoo.com.br>
2023-04-17 14:19:14 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/leandro/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,13 @@
+package com.leandro.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leandro.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+	
+	
+}
